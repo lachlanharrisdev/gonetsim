d19c09e645b41ccf8bab2d2b633dd79bb0459cf6
Lachlan Harris <121010139+lachlanharrisdev@users.noreply.github.com>
2026-04-06 15:33:55 +1000

    fix: remove obsolote escapeXML function

--- internal/httpserver/fake.go
+++ internal/httpserver/fake.go
@@ -195,12 +195,3 @@ func newDefaultFakeRegistry() *fakeRegistry {
 
 	return &fakeRegistry{byExt: byExt, defaultGen: unknownGen}
 }
-
-func escapeXML(s string) string {
-	s = strings.ReplaceAll(s, "&", "&amp;")
-	s = strings.ReplaceAll(s, "<", "&lt;")
-	s = strings.ReplaceAll(s, ">", "&gt;")
-	s = strings.ReplaceAll(s, "\"", "&quot;")
-	s = strings.ReplaceAll(s, "'", "&apos;")
-	return s
-}
