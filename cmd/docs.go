77b49622ace539a977a2caa536254a685e9c57df
Lachlan Harris <121010139+lachlanharrisdev@users.noreply.github.com>
2026-04-08 15:38:03 +1000

    fix: prevent redundant newline errors

--- cmd/docs.go
+++ cmd/docs.go
@@ -10,9 +10,9 @@ var docsCmd = &cobra.Command{
 	Use: "docs",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("GoNetSim's official documentation can be found at https://gonetsim.lachlanharris.dev")
-		fmt.Println("This documentation includes in-depth guides, references for services & detailed specifications\n")
+		fmt.Print("This documentation includes in-depth guides, references for services & detailed specifications\n\n")
 		fmt.Println("The official repository is located at https://github.com/lachlanharrisdev/gonetsim")
-		fmt.Println("This is the source for issue tracking, discussions, downloads and contribution guidelines\n")
+		fmt.Print("This is the source for issue tracking, discussions, downloads and contribution guidelines\n\n")
 	},
 	Short: "Shows link to GoNetSim documentation",
 }
