37b6aff9e36bce5745ef87829a3c30106e0234a8
Lachlan Harris <121010139+lachlanharrisdev@users.noreply.github.com>
2026-04-11 16:28:53 +1000

    feat: add ANON and LOGIN smtp auth (#41)
    * feat: add ANON and LOGIN smtp auth
    
    * fix: balance args in logger.warn
    
    * fix: smtp auth clearing state
    
    * fix: properly set auth bool in AUTH

--- internal/smtpserver/server.go
+++ internal/smtpserver/server.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/emersion/go-sasl"
 	"github.com/emersion/go-smtp"
+	smtputils "github.com/lachlanharrisdev/gonetsim/internal/smtpserver/utils"
 )
 
 type errorLogger struct {
@@ -108,21 +109,54 @@ type Session struct {
 
 // AuthMechanisms returns available authentication mechanisms.
 func (s *Session) AuthMechanisms() []string {
-	return []string{sasl.Plain}
+	return []string{sasl.Plain, sasl.Anonymous, sasl.Login}
 }
 
 // Auth handles authentication.
 func (s *Session) Auth(mech string) (sasl.Server, error) {
-	return sasl.NewPlainServer(func(identity, username, password string) error {
-		s.username = username
-		s.logger.Info("authentication attempt",
-			"remote_addr", s.remoteAddr,
-			"mechanism", mech,
-			"username", username,
-		)
-		s.auth = true
-		return nil
-	}), nil
+	switch mech {
+	case sasl.Plain:
+		return sasl.NewPlainServer(func(identity, username, password string) error {
+			s.username = username
+			s.auth = true
+			s.logger.Info("authentication attempt",
+				"remote_addr", s.remoteAddr,
+				"mechanism", mech,
+			)
+			return nil
+		}), nil
+	case sasl.Anonymous:
+		return sasl.NewAnonymousServer(func(trace string) error {
+			s.username = trace
+			s.auth = true
+			s.logger.Info("authentication attempt",
+				"remote_addr", s.remoteAddr,
+				"mechanism", mech,
+			)
+			return nil
+		}), nil
+	case sasl.Login:
+		return smtputils.NewLoginServer(func(username, password string) error {
+			s.username = username
+			s.auth = true
+			s.logger.Info("authentication attempt",
+				"remote_addr", s.remoteAddr,
+				"mechanism", mech,
+			)
+			return nil
+		}), nil
+	default: // default to ANONYMOUS to hopefully satisfy clients
+		s.logger.Warn("unsupported authentication mechanism; defaulting to ANONYMOUS", "remote_addr", s.remoteAddr, "mechanism", mech)
+		return sasl.NewAnonymousServer(func(trace string) error {
+			s.username = trace
+			s.auth = true
+			s.logger.Info("authentication attempt",
+				"remote_addr", s.remoteAddr,
+				"mechanism", mech,
+			)
+			return nil
+		}), nil
+	}
 }
 
 // Mail handles MAIL FROM command.
@@ -178,8 +212,6 @@ func (s *Session) Data(r io.Reader) error {
 
 // Reset clears the session state.
 func (s *Session) Reset() {
-	s.auth = false
-	s.username = ""
 	s.from = ""
 	s.recipients = []string{}
 	s.logger.Info("session reset",
